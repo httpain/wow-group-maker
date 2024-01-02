79443674d64f21b0f1d41af0c8ba6d66c94f6f4f
Tatiana <tatiana@httpain.com>
2024-01-02 01:19:27 +0000

    Extract scores to a separate file

---
+++ src/main/kotlin/com/httpain/constraints/Scores.kt
@@ -0,0 +1,13 @@
+package com.httpain.constraints
+
+object Scores {
+
+    const val PENALTY_ON_ROLE_MISMATCH = 1000
+    const val PENALTY_ON_DUPLICATE_PLAYERS_IN_GROUP = 1000
+    const val PENALTY_ON_DUPLICATE_PLAYERS_BETWEEN_GROUPS = 1000
+
+    const val PENALTY_BASE_ON_EMPTY_SLOT = 10
+
+    const val REWARD_ON_KEY_RANGE_MATCH = 10000
+
+}
